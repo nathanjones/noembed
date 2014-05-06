7752ea25500bd799c5e20a778352b68fed9180da
Nathan Jones <me@nathanrjones.com>
2014-05-06 00:04:22 -0500

    Updated Clyp pattern to support HTTPS URLs

--- lib/Noembed/Provider/Clyp.pm
+++ lib/Noembed/Provider/Clyp.pm
@@ -3,7 +3,7 @@ package Noembed::Provider::Clyp;
 use parent 'Noembed::oEmbedProvider';
 
 sub provider_name {"Clyp"}
-sub patterns {'http://clyp\.it/.*'}
-sub oembed_url {'http://api.clyp.it/oembed/'}
+sub patterns {'https?://clyp\.it/.*'}
+sub oembed_url {'https://api.clyp.it/oembed/'}
 
 1;
